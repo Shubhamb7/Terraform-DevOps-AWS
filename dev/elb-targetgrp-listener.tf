805adeac5fea3132a262005535a310b5754768bb
Shubhamb7 <shubhamb756@gmail.com>
2022-07-27 16:48:20 +0530

    Added ELB, target group and ELB listener configuration
    - create ELB
    - create target group
    - create ELB listener
    - attach AWS instance to target grp

---
+++ dev/elb-targetgrp-listener.tf
@@ -0,0 +1,47 @@
+resource "aws_lb_target_group" "web_target_grp" {
+  name     = "web-target-grp"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+  target_type = "instance"
+
+  health_check {
+    interval = 10
+    path = "/"
+    protocol = "HTTP"
+    timeout = 5
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg[4].id]
+  subnets = [aws_subnet.public_subnet[0].id,aws_subnet.public_subnet[1].id]
+
+  tags = {
+    Environment = "${var.env}-web-alb"
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_grp.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.web_target_grp.arn
+  target_id        = aws_instance.ec2[1].id
+  port             = 8000
+}
