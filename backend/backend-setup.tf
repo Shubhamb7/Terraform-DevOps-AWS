b3146b311aa5ac1b45a0399d296a84f5172205d1
Shubhamb7 <shubhamb756@gmail.com>
2022-07-27 16:10:04 +0530

    Added backend-setup terraform file
    - created aws terraform provider
    - set region and profile
    - created s3 bucket for terraform state file
    - enabled server side encryption for the bucket
    - enabled versioning
    - created dynamodb table for state locking

---
+++ backend/backend-setup.tf
@@ -0,0 +1,47 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "devops-tf-state-3298"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_sse" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
