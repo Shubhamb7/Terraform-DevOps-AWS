e595ef65ab1f8ca659213b648b0df869be1566ae
Shubhamb7 <shubhamb756@gmail.com>
2022-07-27 16:51:31 +0530

    Added variables file
    - create variables for the dynamic functioning of terraform

---
+++ dev/variables.tf
@@ -0,0 +1,48 @@
+variable "profile" {
+    type = string
+}
+variable "aws_region" {
+    type = string
+}
+variable "env" {
+    type = string
+}
+variable "ami" {
+  type = string
+}
+variable "vpc_cidr_block" {
+    type = string
+}
+variable "public_subnet_cidr" {
+    type = list
+}
+variable "private_subnet_cidr" {
+    type = string
+}
+variable "subnet_availability_zones" {
+    type = list
+}
+variable "instance_type_list" {
+  type = list
+}
+variable "keypair" {
+    type = string
+}
+variable "server_list" {
+  type = list
+}
+variable "instance_subnet_list" {
+  type = list
+}
+variable "user_data_map" {
+  type = map
+}
+variable "sg_list" {
+  type = list
+}
+variable "zone_id" {
+  type = string
+}
+variable "domain_name" {
+  type = string
+}
